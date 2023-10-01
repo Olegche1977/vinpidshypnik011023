b664f1f94a506729b2767e71099c11baee7023d7
Olegche1977 <101511607+Olegche1977@users.noreply.github.com>
2023-10-01 11:45:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import './App.css';
         
         
         <Routes>
-          <Route path=process.env.PUBLIC_URL element={<Layout/>}>
+          <Route path="/" element={<Layout/>}>
             <Route index element={<HomePage/>}/>
             <Route path="bearings" element={<BearingsPage/>}/>
             <Route path="bearings/:id" element={<SingleBearingPage/>}/>
